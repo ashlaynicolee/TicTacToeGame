5f94e9b77e25c97354d24527fc7b37d636492772
Ashleigh Wilcox <awilcox@stu.jsu.edu>
2017-09-20 11:58:44 -0500

    Created and initialized 2D array w/ place mark method

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -2,46 +2,46 @@ package edu.jsu.mcis;
 
 public class TicTacToeView {
 
-    private TicTacToeModel model;
-    
-    /* CONSTRUCTOR */
+   private TicTacToeModel model;
+   
+   /* CONSTRUCTOR */
 	
-    public TicTacToeView(TicTacToeModel model) {
-        
-        this.model = model;
-        
-    }
+   public TicTacToeView(TicTacToeModel model) {
+      
+      this.model = model;
+      
+   }
 	
-    public void viewModel() {
-        
-        /* Print the board to the console (see examples) */
-        
-        /* INSERT YOUR CODE HERE */
+   public void viewModel() {
+      
+      /* Print the board to the console (see examples) */
+      
+      /* INSERT YOUR CODE HERE */
+   
+   }
 
-    }
+   public void showNextMovePrompt() {
+   
+      /* Display a prompt for the player's next move (see examples) */
+   
+      /* INSERT YOUR CODE HERE */
+   
+   }
 
-    public void showNextMovePrompt() {
+   public void showInputError() {
+   
+      /* Display an error if input is invalid (see examples) */
+   
+      /* INSERT YOUR CODE HERE */
+   
+   }
 
-        /* Display a prompt for the player's next move (see examples) */
-
-        /* INSERT YOUR CODE HERE */
-
-    }
-
-    public void showInputError() {
-
-        /* Display an error if input is invalid (see examples) */
-
-        /* INSERT YOUR CODE HERE */
-
-    }
-
-    public void showResult(String r) {
-
-        /* Display final winner */
-
-        System.out.println(r + "!");
-
-    }
+   public void showResult(String r) {
+   
+      /* Display final winner */
+   
+      System.out.println(r + "!");
+   
+   }
 	
 }
--- src/test/java/edu/jsu/mcis/TicTacToeViewTest.java
+++ src/test/java/edu/jsu/mcis/TicTacToeViewTest.java
@@ -7,34 +7,34 @@ import java.io.*;
 
 public class TicTacToeViewTest {
 	
-    private TicTacToeModel model;
-    private TicTacToeView view;
-    private ByteArrayOutputStream output;
+   private TicTacToeModel model;
+   private TicTacToeView view;
+   private ByteArrayOutputStream output;
 
-    @Before
-    public void setup() {
-        
-        model = new TicTacToeModel(3);
-        view = new TicTacToeView(model);
-        output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
-        
-    }
+   @Before
+   public void setup() {
+      
+      model = new TicTacToeModel(3);
+      view = new TicTacToeView(model);
+      output = new ByteArrayOutputStream();
+      System.setOut(new PrintStream(output));
+      
+   }
 	
-    @After
-    public void cleanup() {
-        
-        System.setOut(null);
-        
-    }
+   @After
+   public void cleanup() {
+      
+      System.setOut(null);
+      
+   }
 	
-    @Test
-    public void testViewOfBlankGrid() {
-        
-        view.viewModel();
-        String s = "\n  012\n\n0 ---\n1 ---\n2 ---\n\n\n\n";
-        assertEquals(s.length(), output.size());
-        
-    }
+   @Test
+   public void testViewOfBlankGrid() {
+      
+      view.viewModel();
+      String s = "\n  012\n\n0 ---\n1 ---\n2 ---\n\n\n\n";
+      assertEquals(s.length(), output.size());
+      
+   }
 	
 }
